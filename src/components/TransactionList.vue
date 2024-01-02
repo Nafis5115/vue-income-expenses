cffb6985216c8ad4655ab69e92607270a375cff5
Nafis Shahriar <shaan@Nafiss-MacBook-Air.local>
2024-01-02 22:09:04 +0600

    new project

---
+++ src/components/TransactionList.vue
@@ -0,0 +1,33 @@
+<template>
+    <h3>History</h3>
+
+
+    <ul id="list" class="list" v-if="transactions.length > 0">
+
+        <li v-for="transaction in transactions" :key="transaction.id" :class="transaction.amount < 0 ? 'minus' : 'plus'">
+            {{ transaction.text }} <span>${{ transaction.amount }}</span>
+            <button @click="deleteTransaction(transaction.id)" class="delete-btn">x</button>
+        </li>
+
+
+    </ul>
+    <h1 v-else style="text-align: center; font-size: medium;">No history yet</h1>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+
+const emit = defineEmits(["transactionDeleted"]);
+
+const props = defineProps({
+    transactions: {
+        type: Array,
+        required: true
+    }
+});
+
+const deleteTransaction = (id) => {
+    emit('transactionDeleted', id);
+}
+
+</script>
